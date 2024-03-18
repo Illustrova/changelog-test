b845d0e9a0115e2cb35c94591153cfaf90946e49
Irina Illustrova <illustrova@gmail.com>
2024-03-18 22:57:53 +0200

    feat: add main workspace
    chore: add gitignore
    
    chore: commit lock file
    
    feat(button): add button component
    
    chore(main): release 1.0.0
    
    Release 1.0.1
    
    feat(button): add variant property
    
    set up conventional changelog plugin
    
    add action
    
    fix script
    
    fix config
    
    Release 1.0.2
    
    chore(release): 0.2.1
    
    update deps
    
    upd token
    
    upd token
    
    upd token
    
    chore(release): 0.2.2
    
    chore: add starter changelog
    
    chore(release): 0.2.2
    
    upd changelog
    
    chore(release): 0.2.3
    
    chore(release): 0.2.4
    
    chore: remove comventional changelog plugin
    
    chore(release): 0.2.5
    
    reset branch to starting state

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## 1.0.0 (2024-03-18)
+
+
+### Features
+
+* add main workspace ([64ad6ea](https://github.com/Illustrova/changelog-test/commit/64ad6ea42f84f94f0cddfd271bd0f31ca401c3a2))
+* **button:** add button component ([0d4a4aa](https://github.com/Illustrova/changelog-test/commit/0d4a4aa790aac20ca86fbab82047c5ebf464e180))
---
+++ packages/ui/src/components/Button/Button.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+type ButtonProps = {
+  children: React.ReactNode;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  variant?: "primary" | "secondary";
+};
+
+export const Button = (props: ButtonProps) => {
+  const { children, ...rest } = props;
+  return (
+    <button
+      style={{ color: props.variant === "primary" ? "red" : "inherit" }}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
