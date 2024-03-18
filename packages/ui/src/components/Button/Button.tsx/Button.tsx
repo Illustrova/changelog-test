790b0e848ab9fc351cd9c1fb5677bd683924968d
Irina Illustrova <illustrova@gmail.com>
2024-03-18 23:01:45 +0200

    chore: add gitignore

---
+++ packages/ui/src/components/Button/Button.tsx/Button.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+type ButtonProps = {
+  children: React.ReactNode;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+};
+
+export const Button = (props: ButtonProps) => {
+  const { children, ...rest } = props;
+  return <button {...rest}>{children}</button>;
+};
