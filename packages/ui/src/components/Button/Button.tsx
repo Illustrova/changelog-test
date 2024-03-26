58d211688db842336d55eb069f34709bb8657a98
Irina Illustrova <illustrova@gmail.com>
2024-03-26 11:09:04 +0200

    fix(button): remove button type

--- packages/ui/src/components/Button/Button.tsx
+++ packages/ui/src/components/Button/Button.tsx
@@ -10,7 +10,6 @@ export const Button = (props: ButtonProps) => {
   const { children, ...rest } = props;
   return (
     <button
-      type="button"
       style={{ color: props.variant === "primary" ? "blue" : "inherit" }}
       {...rest}
     >
