2802e0d3f670f02b2b7cb6f1839a4921f2a01fcd
Irina Illustrova <illustrova@gmail.com>
2024-03-28 12:36:04 +0200

    fix(input): disallow email and password types

--- packages/ui/src/components/Input/Input.tsx
+++ packages/ui/src/components/Input/Input.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 type InputProps = {
-  type?: "text" | "password" | "email" | "number" | "tel" | "url" | "search";
+  type?: "text" | "number" | "tel" | "url" | "search";
 };
 
-export const Input = ({ type="text", ...props }: InputProps) => {
+export const Input = ({ type = "text", ...props }: InputProps) => {
   return <input type={type} {...props} />;
 };
